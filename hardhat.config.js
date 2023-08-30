871648fccff2ef8cfeda946717aa49565bd97f4c
felixkuang <562106689@qq.com>
2023-08-30 18:29:15 +0800

    config network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,31 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+
+const { INFURA_API_KEY, SEPOLIA_PRIVATE_KEY } = process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  networks: {
+    sepolia: {
+      chainId: 11155111,
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY]
+    }
+  },
   settings: {
     optimizer: {
       enabled: true,
       runs: 200,
     },
   },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache", 
+    artifacts: "./artifacts"
+  },
+  mocha: {
+    timeout: 40000
+  }
 };
